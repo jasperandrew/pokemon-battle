a2b9c354649dcebea87c3730746bca49ad575938
jasperandrew <jasper@jasperandrew.com>
2016-01-05 23:39:12 -0800

    Integrated all attack animations/calculations into universal attack function.

--- js/game.js
+++ js/game.js
@@ -2,89 +2,78 @@ var currentState;
 var cpuPkmn;
 var playerPkmn;
 
-function attack(user, move) {
-  console.log(user + " attacked with " + move.name + " (" + move.class + ")");
+var attacker, attackerTxt, target, targetTxt, nextTurn;
 
-  var userPkmn, targetPkmn, target, nextTurn;
-  if(user == "player"){
-    userPkmn = playerPkmn;
-    targetPkmn = cpuPkmn;
-    target = "cpu";
-    nextTurn = cpuTurn;
-  }else{
-    userPkmn = cpuPkmn;
-    targetPkmn = playerPkmn;
-    target = "player";
-    nextTurn = playerTurn;
+function attack(move) {
+
+  function animateAttack() {
+    // Run the correct player animation //
+    if(attackerTxt == "player"){
+      $("#player").animate({ left: "+=10px" }, 100, function(){
+        $("#player").animate({ left: "-=10px" }, 100)
+      });
+    }else{
+      $("#cpu").animate({ right: "+=10px" }, 100, function(){
+        $("#cpu").animate({ right: "-=10px" }, 100)
+      });
+    }
+
+    // Run the hit effect //
+    $("#attack").addClass(attackerTxt + "-attack");
+    $("#attack").removeClass("hide");
+    $("#attack").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100);
+    setTimeout(function(){
+      $("#attack").addClass("hide");
+      $("#attack").removeClass(attackerTxt + "-attack");
+    }, 1000);
+
+    setTimeout(calcAttack, 1000);
   }
 
-  $("#attack").addClass("hide");
-  $("#attack").removeClass(user + "-attack");
-
-  if(move.class == "phys" || move.class == "spec"){
-    if(!userPkmn.effect){
-      targetPkmn.health -= move.pwr;
+  // Function to calculate damage/effects //
+  function calcAttack() {
+    if(move.class == "phys" || move.class == "spec"){
+      if(!attacker.effect){
+        target.health -= move.pwr;
+      }else{
+        target.health -= move.pwr + (move.pwr * target.effect);
+        target.effect = null;
+      }
+      $("#" + targetTxt + "-health").animate({
+        width: (target.health/target.max_health)*100 + "%",
+      }, 200);
+      currentState = nextTurn;
+      loop();
     }else{
-      targetPkmn.health -= move.pwr - (move.pwr * targetPkmn.effect);
-      targetPkmn.effect = null;
+      target.effect = move.pwr;
+      currentState = nextTurn;
+      loop();
     }
-    $("#" + target + "-health").animate({
-      width: (targetPkmn.health/targetPkmn.max_health)*100 + "%",
-    }, 200);
-    currentState = nextTurn;
-    loop();
+  }
+
+  // Figure out if the attack will hit //
+  var accuracy = Math.random();
+  if(accuracy <= move.acc){
+    console.log(attackerTxt + " hit with " + move.name + " (" + move.class + ")");
+    $("#action-text").text(move.name + " hit!");
+    animateAttack(); // If the attack hits, continue to animate it //
   }else{
-    targetPkmn.effect = move.pwr;
+    console.log(attackerTxt + " missed with " + move.name + " (" + move.class + ")");
+    $("#action-text").text(move.name + " missed!");
     currentState = nextTurn;
-    loop();
+    setTimeout(loop, 1000); // If the attack misses, exit and go to the next turn //
   }
 }
 
 
 var playerTurn = {
   play: function(){
-    var move;
-
-    function prepAttack() {
-      $("#player-buttons").addClass("hide");
-      $("#player").animate({
-        left: "+=10px",
-      }, 100, function(){
-        $("#player").animate({
-          left: "-=10px",
-        }, 100)
-      })
-      getAccuracy();
-    }
-
-    function getAccuracy() {
-      var accuracy = Math.random();
-      if(accuracy <= move.acc){
-        $("#action-text").text(move.name + " hit!");
-        getMoveClass();
-      }else{
-        console.log("player missed with " + move.name + " (" + move.class + ")");
-        $("#action-text").text(move.name + " missed!");
-        currentState = cpuTurn;
-        setTimeout(loop, 1500);
-      }
-    }
-
-    function getMoveClass() {
-      animateMove();
-      setTimeout(attack, 1500, "player", move);
-    }
-
-    function animateMove() {
-      $("#attack").addClass("player-attack");
-      $("#attack").removeClass("hide");
-      $("#attack").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100);
-    }
-
-    $(".move-button").unbind().click(function(){
-      move = moveList[playerPkmn.moves[$(this).attr("value")]];
-      prepAttack();
-    });
+    // Set the stuff //
+    attacker = playerPkmn;
+    attackerTxt = "player";
+    target = cpuPkmn;
+    targetTxt = "cpu";
+    nextTurn = cpuTurn;
 
     // Set up user buttons //
     var moveButtons = ["#move-1 .move-text", "#move-2 .move-text", "#move-3 .move-text", "#move-4 .move-text"];
@@ -93,50 +82,31 @@ var playerTurn = {
     for(var i = 0; i < 4; i++){
       $(moveButtons[i]).text(moveList[playerPkmn.moves[i]].name);
     }
+
+    // Pick a move to use //
+    var move;
+    $(".move-button").unbind().click(function(){
+      move = moveList[playerPkmn.moves[$(this).attr("value")]];
+      $("#player-buttons").addClass("hide");
+      $("#action-text").text(playerPkmn.name + " used " + move.name + "!");
+      setTimeout(attack, 1000, move);
+    });
   }
 };
 
 var cpuTurn = {
   play: function(){
+    // Set the stuff //
+    attacker = cpuPkmn;
+    attackerTxt = "cpu";
+    target = playerPkmn;
+    targetTxt = "player";
+    nextTurn = playerTurn;
+
+    // Randomly select a move to use //
     var move = moveList[cpuPkmn.moves[Math.floor(Math.random() * 4)]];
     $("#action-text").text("Enemy " + cpuPkmn.name + " used " + move.name + "!");
-
-    function prepAttack() {
-      $("#cpu").animate({
-        right: "+=10px",
-      }, 100, function(){
-        $("#cpu").animate({
-          right: "-=10px",
-        }, 100)
-      })
-      getAccuracy();
-    }
-
-    function getAccuracy() {
-      var accuracy = Math.random();
-      if(accuracy <= move.acc){
-        $("#action-text").text(move.name + " hit!");
-        getMoveClass();
-      }else{
-        console.log("cpu missed with " + move.name + " (" + move.class + ")");
-        $("#action-text").text(move.name + " missed!");
-        currentState = playerTurn;
-        setTimeout(loop, 1500);
-      }
-    }
-
-    function getMoveClass() {
-      animateMove();
-      setTimeout(attack, 1500, "cpu", move);
-    }
-
-    function animateMove() {
-      $("#attack").addClass("cpu-attack");
-      $("#attack").removeClass("hide");
-      $("#attack").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100);
-    }
-
-    setTimeout(prepAttack, 1500);
+    setTimeout(attack, 1000, move);
   }
 };
 
